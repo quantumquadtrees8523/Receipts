982d4d5ffbc5fffc13f7ae41df53aa0a692af6ed
Surya Duggirala <suryaduggirala@Suryas-MacBook-Pro.local>
2021-11-20 19:18:28 -0800

    basic homepage is set up

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { DigtalReciept } from './src/components/DigitalReceipt';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <DigtalReciept categoryTags={['test']}/>
     </View>
   );
 }
@@ -14,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000080',
     alignItems: 'center',
     justifyContent: 'center',
   },
